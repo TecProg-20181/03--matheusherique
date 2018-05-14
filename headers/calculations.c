6d7534c74e777b1a98a77f6513c5ee3b338d3120
Matheus Costa <matheus.bionicle@gmail.com>
2018-05-14 18:39:45 -0300

    Add similar try/catch to C and add log system

--- headers/calculations.c
+++ headers/calculations.c
@@ -1,19 +1,29 @@
 /**
-* This file is part of Photopobre project.
-* @file calculations.c
-* @brief This file include all functions used for the calcules of project.
-*
-*/
+ * This file is part of Photopobre project.
+ * @file calculations.c
+ * @brief This file include all functions used for the calcules of project.
+ *
+ */
 
+#include <setjmp.h>
 #include "calculations.h"
+#include "log.h"
+
+static jmp_buf s_jumpBuffer;
 
 int minimum(int a, int b) {
         int which_input = 0;
 
-        if (a < b) {
-                which_input = a;
+        if (!setjmp(s_jumpBuffer)) {
+                // try
+                if (a < b) {
+                        which_input = a;
+                } else {
+                        which_input = b;
+                }
         } else {
-                which_input = b;
+                // catch
+                log_error("An error occurred!");
         }
 
         return which_input;
@@ -23,10 +33,16 @@ int minimum(int a, int b) {
 int maximum(int a, int b) {
         int which_input = 0;
 
-        if (a > b) {
-                which_input = a;
+        if (!setjmp(s_jumpBuffer)) {
+                // try
+                if (a > b) {
+                        which_input = a;
+                } else {
+                        which_input = b;
+                }
         } else {
-                which_input = b;
+                // catch
+                log_error("An error occurred!");
         }
 
         return which_input;
